0eb1487028038bbc146a779df4ab1f9d6cf33aaf
Antoine Fissot <antoine@semifir.com>
2020-12-01 13:47:03 +0100

    c9

--- toto.js
+++ toto.js
@@ -7,3 +7,21 @@ let e = 4
 () => {
     console.log('test')
 }
+const test = "java"
+const z = () => {
+    console.log("test")
+}
+const x = 5
+let e = 4
+() => {
+    console.log('test')
+}
+const test = "java"
+const z = () => {
+    console.log("test")
+}
+const x = 5
+let e = 4
+() => {
+    console.log('test')
+}
